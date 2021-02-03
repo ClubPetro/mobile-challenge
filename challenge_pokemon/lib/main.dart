8cf5a6dc56462d64295b69ca709a357f7f1dffb3
Caio Caldeira <caiocaldeira2013@gmail.com>
2021-02-02 22:11:45 -0300

    Update 8 - 14
    main.dart for Android

--- challenge_pokemon/lib/main.dart
+++ challenge_pokemon/lib/main.dart
@@ -1,8 +1,8 @@
-//import 'package:flutter/material.dart';
-//import 'app/views/android/app.widget.dart';
+import 'package:flutter/material.dart';
+import 'app/views/android/app.widget.dart';
 
-import 'package:flutter/cupertino.dart';
-import 'app/views/ios/app.widget.dart';
+//import 'package:flutter/cupertino.dart';
+//import 'app/views/ios/app.widget.dart';
 
 void main() {
   runApp(AppWidget());
